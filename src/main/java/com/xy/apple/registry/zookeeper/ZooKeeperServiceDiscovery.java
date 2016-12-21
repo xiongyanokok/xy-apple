11fad81afd47647f7f901e717c8d49d63b46b0b8
熊焱 <xiongyan-123@163.com>
2016-12-21 11:02:24 +0800

    优化zk
    优化zk

--- src/main/java/com/xy/apple/registry/zookeeper/ZooKeeperServiceDiscovery.java
+++ src/main/java/com/xy/apple/registry/zookeeper/ZooKeeperServiceDiscovery.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
+import com.xy.apple.exception.ParamNotFoundException;
 import com.xy.apple.registry.ServiceDiscovery;
 
 /**
@@ -15,9 +19,11 @@ import com.xy.apple.registry.ServiceDiscovery;
  * @author xiongyan
  * @since 1.0.0
  */
-public class ZooKeeperServiceDiscovery implements ServiceDiscovery {
+public class ZooKeeperServiceDiscovery implements ServiceDiscovery, InitializingBean, DisposableBean {
 
 	private static final Logger logger = LoggerFactory.getLogger(ZooKeeperServiceDiscovery.class);
+	
+	private ZkClient zkClient;
 
 	private String zookeeperAddress;
 
@@ -28,37 +34,47 @@ public class ZooKeeperServiceDiscovery implements ServiceDiscovery {
 	public void setZookeeperAddress(String zookeeperAddress) {
 		this.zookeeperAddress = zookeeperAddress;
 	}
-
-	public String discover(String serviceName) {
+	
+	
+	public void afterPropertiesSet() throws Exception {
+		if (StringUtils.isEmpty(zookeeperAddress)) {
+			throw new ParamNotFoundException("zookeeperAddress not found ");
+		}
+		
 		// 创建 ZooKeeper 客户端
-		ZkClient zkClient = new ZkClient(zookeeperAddress, Constant.ZK_SESSION_TIMEOUT, Constant.ZK_CONNECTION_TIMEOUT);
+		zkClient = new ZkClient(zookeeperAddress, Constant.ZK_SESSION_TIMEOUT, Constant.ZK_CONNECTION_TIMEOUT);
 		logger.debug("connect zookeeper");
-		try {
-			// 获取 service 节点
-			String servicePath = Constant.ZK_REGISTRY_PATH + "/" + serviceName;
-			if (!zkClient.exists(servicePath)) {
-				throw new RuntimeException(String.format("can not find any service node on path: %s", servicePath));
-			}
-			List<String> addressList = zkClient.getChildren(servicePath);
-			if (null == addressList || addressList.size() == 0) {
-				throw new RuntimeException(String.format("can not find any address node on path: %s", servicePath));
-			}
-			// 获取 address 节点
-			String address;
-			int size = addressList.size();
-			if (size == 1) {
-				// 若只有一个地址，则获取该地址
-				address = addressList.get(0);
-				logger.debug("get only address node: {}", address);
-			} else {
-				// 若存在多个地址，则随机获取一个地址
-				address = addressList.get(ThreadLocalRandom.current().nextInt(size));
-				logger.debug("get random address node: {}", address);
-			}
-			// 获取 address 节点的值
-			String addressPath = servicePath + "/" + address;
-			return zkClient.readData(addressPath);
-		} finally {
+	}
+	
+	public String discover(String serviceName) {
+		// 获取 service 节点
+		String servicePath = Constant.ZK_REGISTRY_PATH + "/" + serviceName;
+		if (!zkClient.exists(servicePath)) {
+			throw new RuntimeException(String.format("can not find any service node on path: %s", servicePath));
+		}
+		List<String> addressList = zkClient.getChildren(servicePath);
+		if (null == addressList || addressList.size() == 0) {
+			throw new RuntimeException(String.format("can not find any address node on path: %s", servicePath));
+		}
+		// 获取 address 节点
+		String address;
+		int size = addressList.size();
+		if (size == 1) {
+			// 若只有一个地址，则获取该地址
+			address = addressList.get(0);
+			logger.debug("get only address node: {}", address);
+		} else {
+			// 若存在多个地址，则随机获取一个地址
+			address = addressList.get(ThreadLocalRandom.current().nextInt(size));
+			logger.debug("get random address node: {}", address);
+		}
+		// 获取 address 节点的值
+		String addressPath = servicePath + "/" + address;
+		return zkClient.readData(addressPath);
+	}
+	
+	public void destroy() throws Exception {
+		if (null != zkClient) {
 			zkClient.close();
 		}
 	}
